072cbfaa39396e4b88ede4a42909789a4f3af9a6
shagun0061 <shagun08081999@gmail.com>
2022-06-14 21:15:28 -0700

    update

--- navbar.js
+++ navbar.js
@@ -1,19 +1,18 @@
-
 document.querySelector('#centerLogo').addEventListener('click', home);
 document.querySelector('#cart').addEventListener('click', cart);
 document.querySelector('#profile').addEventListener('click', profile);
 // function for home page redirection
 function home() {
     console.log('home page')
-    // window.location.href='index.html'
+        // window.location.href='index.html'
 }
 // functin for cart page redirection
 function cart() {
     console.log('cart page')
-    // window.location.href='cartPage.html'
+        // window.location.href='cartPage.html'
 }
 // functin for profile page redirection
 function profile() {
     console.log('profile page')
-    // window.location.href='profilePage.html'
+        // window.location.href='profilePage.html'
 }
