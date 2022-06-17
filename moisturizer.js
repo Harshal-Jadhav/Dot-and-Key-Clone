3cf645ea1094cc2f9010b31f781c3a92862c2e20
Rituraj <rajvermaritu15@gmail.com>
2022-06-17 17:31:03 +0530

    update

--- moisturizer.js
+++ moisturizer.js
@@ -213,4 +213,23 @@ function changefunction() {
 }
 
 
-///
+///here to start navbar js start;
+
+document.querySelector('#centerLogo').addEventListener('click', home);
+document.querySelector('#cart').addEventListener('click', cart);
+document.querySelector('#profile').addEventListener('click', profile);
+// function for home page redirection
+function home() {
+    console.log('home page')
+    // window.location.href='index.html'
+}
+// functin for cart page redirection
+function cart() {
+    console.log('cart page')
+    // window.location.href='cartPage.html'
+}
+// functin for profile page redirection
+function profile() {
+    console.log('profile page')
+    // window.location.href='profilePage.html'
+}
